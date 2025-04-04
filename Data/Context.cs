54423cbe0830c47bec1bacc052118d42a0c697d0
dvaun <david@dvaun.com>
2025-04-04 10:18:58 -0700

    Add back navbar; add in company views/base

--- Data/Context.cs
+++ Data/Context.cs
@@ -9,5 +9,33 @@ namespace Data.DB
         public DbSet<MemberCompany> MemberCompanies { get; set;}
 
         public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) {
+            builder.UseSeeding((context, _) =>
+            {
+                var baseCompany = context.Set<MemberCompany>().FirstOrDefault(c => c.Id == -1);
+                if (baseCompany == null)
+                {
+                    context.Set<MemberCompany>().Add(new MemberCompany {
+                        Id = -1,
+                        MembershipAcronym = "",
+                        Name = "Unknown"
+                    });
+                    context.SaveChanges();
+                }
+            }).UseAsyncSeeding(async (context, _, cancellationToken) =>
+            {                
+                var baseCompany = await context.Set<MemberCompany>().FirstOrDefaultAsync(c => c.Id == -1, cancellationToken);
+                if (baseCompany == null)
+                {
+                    context.Set<MemberCompany>().Add(new MemberCompany {
+                        Id = -1,
+                        MembershipAcronym = "",
+                        Name = "Unknown"
+                    });
+                    await context.SaveChangesAsync(cancellationToken);
+                }
+            });
+        }
     }
 }
