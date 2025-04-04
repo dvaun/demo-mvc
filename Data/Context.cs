e982c28e0dc1ef0985974fa40507f770856287e5
dvaun <david@dvaun.com>
2025-04-04 10:00:24 -0700

    add company model

--- Data/Context.cs
+++ Data/Context.cs
@@ -6,6 +6,7 @@ namespace Data.DB
     public class DataContext : DbContext
     {
         public DbSet<Member> Members { get; set; }
+        public DbSet<MemberCompany> MemberCompanies { get; set;}
 
         public DataContext(DbContextOptions<DataContext> options) : base(options) {}
     }
