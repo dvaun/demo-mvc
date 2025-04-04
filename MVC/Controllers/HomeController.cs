2e9a50c1c2102d5110e48e823ee1d392a6f7f763
dvaun <david@dvaun.com>
2025-04-03 22:25:58 -0700

    remove commented data; sort by date of birth

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -19,46 +19,10 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        // var members = new List<Member> {
-        //     new Member
-        //     {
-        //         FirstName = "Willie",
-        //         LastName = "Nelson",
-        //         Address = null,
-        //         PhoneNumber = "555-9012",
-        //         DOB = new DateTime(1933, 4, 29),
-        //         MembershipID = "FL1234"
-        //     },
-        //     new Member
-        //     {
-        //         FirstName = "Jayden",
-        //         LastName = "Smith",
-        //         Address = "ABC Hollywood Ln",
-        //         PhoneNumber = "279-1234",
-        //         DOB = new DateTime(1999, 7, 8),
-        //         MembershipID = "XYZ2345"
-        //     },
-        //     new Member
-        //     {
-        //         FirstName = "Bill",
-        //         LastName = "Gates",
-        //         Address = "Some Rich Road",
-        //         PhoneNumber = "123-4567",
-        //         DOB = new DateTime(1956, 10, 28),
-        //         MembershipID = "RICH1234"
-        //     },
-        //     new Member
-        //     {
-        //         FirstName = "David",
-        //         LastName = "Lynch",
-        //         Address = "High Heaven Blvd",
-        //         PhoneNumber = "987-6543",
-        //         DOB = new DateTime(1946, 1, 20),
-        //         MembershipID = "HEAV1234"
-        //     }
-        // };
         var memberViewModel = new MemberListViewModel {
-            Members = _memberService.GetMembers().ToList()
+            Members = _memberService.GetMembers()
+                                    .OrderBy(memberViewModel => member.DOB)
+                                    .ToList()
         };
         return View("Index", memberViewModel);
     }
